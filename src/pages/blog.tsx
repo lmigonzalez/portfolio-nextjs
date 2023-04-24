ca835fb1959d3c2d6d18f504237c2b8164007b7f
lmigonzalez <lmigonzalezs@gmail.com>
2023-04-24 17:56:41 -0400

    error fix 2

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -116,8 +116,8 @@ export default Blog;
 
 export async function getStaticProps() {
   const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID || '',
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY || '',
+    space: process.env.CONTENTFUL_SPACE_ID || 'default',
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY || 'default',
   });
 
   const res = await client.getEntries({ content_type: 'blogPost' });
