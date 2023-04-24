562567b6ed890a2dbf36d1a114f58304ababf04d
lmigonzalez <lmigonzalezs@gmail.com>
2023-04-24 18:11:01 -0400

    env change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-DFT9G3SC34`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MEASUREMENT_ID}`}
       />
 
       <Script id="google-analytics" strategy="afterInteractive">
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-DFT9G3SC34');
+        gtag('config', '${process.env.MEASUREMENT_ID}');
     `}
       </Script>
 
